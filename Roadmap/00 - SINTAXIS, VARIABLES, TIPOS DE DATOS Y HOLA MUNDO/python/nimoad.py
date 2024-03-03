30a7d52929e01816a49b232b9c7900489f671e93
nimoad <122557792+nimoad@users.noreply.github.com>
2024-03-03 17:35:51 +0000

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/nimoad.py
@@ -0,0 +1,44 @@
+#https://www.python.org/
+
+#Comentario en una línea
+
+"""
+Comentario en
+varias líneas
+"""
+
+'''
+Esto también
+es un comentario
+en varias líneas
+'''
+
+my_variable = 'nimoad'
+MY_CONSTANT = 'Mi_Constante' 
+
+'''
+Las constantes en Python no existen. Para guardar un valor constante, 
+utilizamos una variable que por convención se utilizan las letras mayúsculas para darle nombre a dicha variable.
+'''
+
+my_string = 'Mi cadena de texto'
+print(type(my_string))
+print(my_string)
+
+my_int = 33
+print(type(my_int))
+print(my_int)
+
+my_float = 3.3
+print(type(my_float))
+print(my_float)
+
+my_bool = True
+print(type(my_bool))
+print(my_bool)
+
+my_bool = False
+print(type(my_bool))
+print(my_bool)
+
+print("Hola, Python")
